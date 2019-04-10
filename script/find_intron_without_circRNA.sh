f8a93c328635cb34e14b49d5dd0e2c5d03e5acc8
Peisen Sun <sps@snnu.edu.cn>
2019-04-10 17:28:03 +0800

    Update find_intron_without_circRNA.sh

--- script/find_intron_without_circRNA.sh
+++ script/find_intron_without_circRNA.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 # Usage: ./sh find_intron_without_circRNA.sh genome.gff/bed circRNA.gff/bed 
+if [ ! $1 ];then
+	echo 'please input genome bed file'
+	exit
+else
+	pass
+fi
+if [ ! $2 ];then
+        echo 'please input circRNA bed file'
+	exit
+else
+        pass
+fi
+
 # extra exon and gene
 grep 'exon' $1 > exon.gff
 grep 'gene' $1 > gene.gff
 
-# extra intron
 bedtools subtract -a gene.gff -b exon.gff > intron.gff
-
-# find intron covered circRNA
-bedtools interset -a intron.gff -wa > result
-
-# find intron without circRNA
-diff intron.gff result | grep '<' > intron_without_circRNA.gff
-
-# remove tmp file
 rm exon.gff
+bedtools intersect -a intron.gff -wa > result
+diff intron.gff result | grep '<' > intron_without_circRNA.gff
 rm intron.gff
 rm result
