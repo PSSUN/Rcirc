79ac35080e424fb794b5cc0f2b09ae534397aa0b
Peisen Sun <sps@snnu.edu.cn>
2020-09-03 14:50:15 +0800

    Update showCodon.R

--- R/showCodon.R
+++ R/showCodon.R
@@ -4,19 +4,19 @@
 ##' @param x
 
 showCodon <- function(x) {
-  circ <- readDNAStringSet(x)
-  
-  codon <- trinucleotideFrequency(circ)
-  codon <- data.frame(codon)
-  codon <- apply(codon, MARGIN = 2, sum)
-  codon <- sort(x, decreasing = TRUE)
-  
-  codon <- data.frame(codon, type = names(codon))
-  x <- codon$type
-  y <- codon$codon
-  ggplot(codon) +
-    geom_bar(stat = "identity", aes(x = x, y = y), fill = "lightblue") +
-    coord_flip() + scale_x_discrete(limits = rev(x)) +
-    labs(title = "Distribution of 3-nt pattern", x = "Type", y = "Count") +
-    theme(plot.title = element_text(hjust = 0.5))
+circ <- readDNAStringSet(x)
+codon <- trinucleotideFrequency(circ)
+codon <- data.frame(codon)
+codon <- apply(codon, MARGIN = 2, sum)
+
+codon <- data.frame(codon, type = names(codon))
+codon <- codon[sort(codon$codon,index.return=TRUE)$ix,]
+x <- codon$type
+y <- codon$codon
+ggplot(codon) +
+  geom_bar(stat = "identity", aes(x = x, y = y), fill = "lightblue") +
+  coord_flip() + scale_x_discrete(limits = rev(x)) +
+  labs(title = "Distribution of 3-nt pattern", x = "Type", y = "Count") +
+  theme(plot.title = element_text(hjust = 0.5))
+
 }
